11f5b23a605cd4afcdef4f6e9038dd32c8879070
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-06-18 22:21:10 -0500

    Configure sequelize

---
+++ src/migrations/20220619024529-create-workplace.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('workplaces', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(128)
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.TEXT('tiny')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  // eslint-disable-next-line no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('workplaces');
+  }
+};
---
+++ src/models/workplace.js
@@ -0,0 +1,39 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Workplace extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // eslint-disable-next-line no-unused-vars
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Workplace.init({
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      type: DataTypes.BIGINT
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING(128)
+    },
+    description: {
+      allowNull: true,
+      type: DataTypes.TEXT('tiny')
+    }
+  }, {
+    sequelize,
+    timestamps: true,
+    tableName: 'workplaces',
+    modelName: 'Workplace',
+  });
+
+  return Workplace;
+};
